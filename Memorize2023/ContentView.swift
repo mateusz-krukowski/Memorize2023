791d4bef33d6e5e694b8fa1ddb3f4b4d57a17e87
Mateusz Krukowski <mateusz_krukowski@yahoo.com>
2026-07-05 20:19:34 +0200

    Initial Commit

---
+++ Memorize2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize2023
+//
+//  Created by Mateusz on 05/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize2023/Memorize2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2023App.swift
+//  Memorize2023
+//
+//  Created by Mateusz on 05/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
